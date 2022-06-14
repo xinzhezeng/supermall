5b6b837b7126a311daea32b246ee9ca01a904e4e
zxzjames <1043263923@qq.com>
2022-06-14 23:45:03 +0800

    first

--- src/App.vue
+++ src/App.vue
@@ -4,13 +4,12 @@
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </nav>
-    <router-view/>
+    <router-view />
   </div>
 </template>
 
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
